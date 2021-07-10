80f121a48dd78d746d54d5cdb0d9960f917a1155
MaximKononikhin <warelstupid@gmail.com>
2021-07-10 17:25:53 +0300

    init webpack config

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,23 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { jsx, css } from '@emotion/react'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const style = css`
+  color: hotpink;
+`
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import kotik from './assets/images/kotik.jpg';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1 css={style}>Ku</h1>
+            <img src={kotik} alt="" />
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>,
+    document.getElementById('app'),
+)
---
+++ client/webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            chunks: 'all',
+            maxInitialRequests: Infinity,
+            minSize: 0,
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name(module) {
+                        // получает имя, то есть node_modules/packageName/not/this/part.js
+                        // или node_modules/packageName
+                        const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            
+                        // имена npm-пакетов можно, не опасаясь проблем, использовать 
+                        // в URL, но некоторые серверы не любят символы наподобие @
+                        return `npm.${packageName.replace('@', '')}`;
+                    },
+                },
+            },
+        },
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            'react-dom$': 'react-dom/profiling',
+            'scheduler/tracing': 'scheduler/tracing-profiling',
+          }
+      
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+            title: 'Cloud'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_mobules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_mobules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-typescript']
+                    }
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                type: 'asset/resource',
+            },
+        ]
+    }
+};
